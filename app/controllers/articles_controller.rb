01a83479d86afa886a124d8c3c761fe0bea207b9
Roman Serediuk <box4rom@gmail.com>
2022-12-23 12:07:18 +0200

    implement article edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   def show
     @article = Article.find(params[:id])
   end
@@ -11,6 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -21,4 +26,15 @@ class ArticlesController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article has been updated successfully."
+      redirect_to @article
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
 end
