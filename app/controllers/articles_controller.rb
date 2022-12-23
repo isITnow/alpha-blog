038d2d1da2292aa37a93d7e1c47b89997260dc66
Roman Serediuk <box4rom@gmail.com>
2022-12-23 15:19:42 +0200

    add article_params method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article has been updated successfully."
       redirect_to @article
     else
@@ -43,6 +43,10 @@ class ArticlesController < ApplicationController
   private 
     def set_article
       @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
     end
 
 end
