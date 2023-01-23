d5b9552694679773a49693128a6002ac7b2a658b
Roman Serediuk <box4rom@gmail.com>
2023-01-23 17:27:51 +0200

    add categories resourses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
-
   get "about", to: "pages#about"
+
   resources :articles
 
   get "signup", to: "users#new"
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
+
+  resources :categories, expect: [:destroy]
 end
