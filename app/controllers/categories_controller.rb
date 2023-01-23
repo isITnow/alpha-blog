400a5f780112f1fa2cf3a4ea857fabdfa29b6552
Roman Serediuk <box4rom@gmail.com>
2023-01-23 18:06:00 +0200

    implement creation of new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,8 +3,25 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash.notice = "Category was successfully created"
+      redirect_to @category
+    else
+      render "new", status: :unprocessable_entity
+    end
   end
 
   def show
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
