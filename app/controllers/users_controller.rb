1f5e6b1dd588257640cc3e9a9d2dd2483e38bfc7
Roman Serediuk <box4rom@gmail.com>
2023-01-25 11:49:02 +0200

    add password_confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,12 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(
+      :username,
+      :email,
+      :password,
+      :password_confirmation,
+    )
   end
 
   def require_same_user
