5d62bdb9d42b4a4d3f8b25533dbfb31009e9ffc0
Roman Serediuk <box4rom@gmail.com>
2023-02-02 10:18:49 +0200

    update all articles order in index method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,10 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 5)
+    @articles =
+      Article.paginate(page: params[:page], per_page: 5).order(
+        "updated_at DESC",
+      )
   end
 
   def new
